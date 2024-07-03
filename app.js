79e440a5c3a048bd77974cbf38c89ae739799145
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2024-07-03 07:45:13 +0700

    first commit

--- app.js
+++ app.js
@@ -2,21 +2,18 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 
-
 const PORT = 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-
+app.use(express.static('public')); // Serve static files from the public directory
 
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.sendFile(__dirname + '/public/index.html'); // Serve the index.html file
 });
-
 
 server.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
